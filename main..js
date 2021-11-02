7cef0d6f6efd2c644ae2144dfa470a7813551737
Vinicius Pereira <vguard.2003@gmail.com>
2021-11-02 13:21:35 -0300

    Contador para metro

---
+++ main..js
@@ -0,0 +1,19 @@
+let count = 0
+let res = document.getElementById("count-people")
+
+function increment() {
+    count += 1
+    res.innerHTML = `${count}`
+}
+
+function decrement() {
+    count -= 1
+    res.innerHTML = `${count}`
+}
+
+
+function reset() {
+    count = 0
+    res.innerHTML = `${count}`
+}
+
